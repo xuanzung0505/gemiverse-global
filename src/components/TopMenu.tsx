ecce33b5c7f964d352abf951882324d2f80871a7
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2025-08-19 11:36:48 +0700

    feat: enhance TopMenu component with improved styling and responsiveness

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -6,19 +6,22 @@ import GamesIcon from "@mui/icons-material/Games";
 
 function TopMenu() {
   return (
-    <div className="flex bg-sky-600 items-center drop-shadow-2xl sticky top-0 z-1 sm:px-10 gap-2 md:gap-20 justify-center">
+    <div
+      className={`flex bg-sky-600 items-center drop-shadow-2xl sticky top-0 z-1 sm:px-10 gap-2 
+        md:gap-20 justify-center`}
+    >
       <div className="flex grow-1 justify-center">
         <Link href="/">
-          <IconButton>
-            <GamesIcon fontSize="large" />
+          <IconButton className="group">
+            <GamesIcon fontSize="large" className="group-hover:text-white" />
           </IconButton>
         </Link>
       </div>
       <div className="grow-4 flex justify-center sm:justify-center gap-10 sm:gap-24">
-        <Link href="/" className="font-bold hover:text-white duration-100">
+        <Link href="/" className="font-bold hover:text-white">
           Career
         </Link>
-        <Link href="/" className="font-bold hover:text-white duration">
+        <Link href="/" className="font-bold hover:text-white">
           About us
         </Link>
       </div>
