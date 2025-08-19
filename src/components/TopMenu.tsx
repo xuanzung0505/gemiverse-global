7ca34d774e6b523ef626fbb968a5cedbf2b9d184
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2025-08-19 11:29:29 +0700

    feat: improve TopMenu layout and styling for enhanced responsiveness

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -2,23 +2,25 @@
 import * as React from "react";
 import Link from "next/link";
 import { IconButton } from "@mui/material";
-import GamesIcon from '@mui/icons-material/Games';
+import GamesIcon from "@mui/icons-material/Games";
 
 function TopMenu() {
   return (
-    <div className="flex bg-sky-600 items-center px-1 drop-shadow-2xl sticky top-0 z-1">
-      <div className="flex-1">
+    <div className="flex bg-sky-600 items-center drop-shadow-2xl sticky top-0 z-1 sm:px-10 gap-2 md:gap-20 justify-center">
+      <div className="flex grow-1 justify-center">
         <Link href="/">
           <IconButton>
             <GamesIcon fontSize="large" />
           </IconButton>
         </Link>
       </div>
-      <div className="flex-1">
-        <Link href="/" className="font-bold">Career</Link>
-      </div>
-      <div className="flex-1">
-        <Link href="/" className="font-bold">About us</Link>
+      <div className="grow-4 flex justify-center sm:justify-center gap-10 sm:gap-24">
+        <Link href="/" className="font-bold hover:text-white duration-100">
+          Career
+        </Link>
+        <Link href="/" className="font-bold hover:text-white duration">
+          About us
+        </Link>
       </div>
     </div>
   );
